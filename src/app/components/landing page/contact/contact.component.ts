1157fbc574b471f378b240cc06818b9e88aa2a72
aboamin <eeaarrssnn40@gmail.com>
2025-02-23 21:20:19 +0200

    sec

--- src/app/components/landing page/contact/contact.component.ts
+++ src/app/components/landing page/contact/contact.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms'
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [FormsModule , ReactiveFormsModule],
+  imports: [FormsModule , ReactiveFormsModule , NgClass],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+  show:boolean = false
 
   form:FormGroup = new FormGroup ({
     name: new FormControl('' , [Validators.required , Validators.minLength(3)]),
@@ -26,7 +29,11 @@ export class ContactComponent {
     console.log();
     
     emailjs.send('service_febam5i' , 'template_btxeyaa' , {...this.form.value} , {publicKey: 'Ut7Vw1tKZhbyB8wXp'}).then(res =>{
-      console.log(res);
+      this.show = true
+
+      setTimeout(() => {
+        this.show = false
+      }, 2000);
     })
     
   }
